0664bc47356329257f597c84a26551fa83907967
Arthur Negro Lopez <93842112+lopezz78@users.noreply.github.com>
2025-11-21 19:50:13 -0300

    Update jogo da velha
    jogador x jogador concluido
    mudei a logica de ler casa do jogador, reduzindo bem o codigo
    toda a logica de casa invalida ou repitida esta funcionando
    falta fazer jxc agora

--- Jogo_da_Velha.asm
+++ Jogo_da_Velha.asm
@@ -14,10 +14,12 @@ FIM_PROG MACRO
     INT 21H
 ENDM
 
-IMP_STRING MACRO MSG     
+IMP_STRING MACRO MSG
+    PUSH_ALL     
     MOV AH, 9
     LEA DX, MSG
     INT 21H
+    POP_ALL
 ENDM
 
 IMP_TABULEIRO MACRO
@@ -25,6 +27,7 @@ IMP_TABULEIRO MACRO
 ENDM
 
 LIMPA_TELA MACRO
+    PUSH_ALL
     MOV AH, 06h       ; Função 06h: scroll up (rolar para cima)
     MOV AL, 00h       ; 0 = limpa todas as linhas
     MOV BH, 07h       ; Atributo da cor (texto cinza, fundo preto)
@@ -38,6 +41,7 @@ LIMPA_TELA MACRO
     MOV DH, 00h       ; Linha 0
     MOV DL, 00h       ; Coluna 0
     INT 10h
+    POP_ALL
 ENDM
 
 PUSH_ALL MACRO
@@ -56,11 +60,15 @@ POP_ALL MACRO
     POP AX
 ENDM
 
+WIN_X_OU_O MACRO
+
+ENDM
+
 .model SMALL
 .STACK 100H
 .DATA
-    N EQU 3
-    TABULEIRO DB N DUP (N DUP ('_') )
+    TABULEIRO DB '1', '2', '3','4','5','6' ,'7', '8', '9'
+
     MSG_ENFEITE DB '=----------------------------------------------------------------=$'
     MSG_JOGO db "                          Jogo da Velha", 10,13,'$'
     MODO_JOGO db "Deseja Jogar contra Quem?",10,13,'$'
@@ -70,7 +78,10 @@ ENDM
     TURNO_CIRCULO DB 'Turno do Circulo: $'
     TURNO_XIS DB  'Turno do Xis: $'
     quebralinha db 10,13,'$'
-    VITORIA DB 'Vitoria!!$'
+    VITORIA_XIS DB 'Vitoria do Xis!$'
+    VITORIA_CIRCULO DB 'Vitoria do Circulo!$'
+    INVALIDO DB 'Casa Invalida! (Escolha entre 1-9)',10,13,'(Pressione Qualquer Tecla para Retornar)$'
+    REPETIDA DB 'Esta Casa ja foi selecionada',10,13,'(Pressione Qualquer Tecla para Retornar)$'
     
 
 .CODE
@@ -79,7 +90,8 @@ MAIN PROC
     INI_DS
 
 TELA_INICIAL:
-
+; INTERFACE DA TELA INICIAL
+    LIMPA_TELA
     IMP_STRING MSG_ENFEITE
     IMP_STRING MSG_JOGO
     IMP_STRING MSG_ENFEITE
@@ -94,6 +106,8 @@ TELA_INICIAL:
     JE JXJ
     CMP AL, '2'
     JE JXC
+
+; SE NAO OBEDECER NENHUMA DAS CONDICOES, RETORNA PARA A PAGINA INICIAL
     LIMPA_TELA
     IMP_STRING ERRO
     LER_CARAC
@@ -113,25 +127,28 @@ MAIN ENDP
 
 JOGADOR_X_JOGADOR PROC ;(Jogador x Jogador)
 
-INICIO:
-    CALL IMPRIME_TAB_INI
-
-RODADA_XIS:   
+; SEPARADO EM TURNOS DE CADA JOGADOR
+RODADA_XIS:
+    MOV DL, 'X'
+    CALL LER_JOGADA
     LIMPA_TELA
     CALL IMPRIME_TAB
-    IMP_STRING TURNO_XIS
-    CALL LER_JOGADA_X
+    CALL WIN_CHECK ; VERIFICA SE HOUVE VENCEDOR
+    JNZ FIM_DE_JOGO
 
 RODADA_CIRCULO:
+    MOV DL, 'O'
+    CALL LER_JOGADA
     LIMPA_TELA
     CALL IMPRIME_TAB
-    IMP_STRING TURNO_CIRCULO
-    CALL LER_JOGADA_O
     CALL WIN_CHECK ; VERIFICA SE HOUVE VENCEDOR
-    JNZ FIM
+    JNZ FIM_DE_JOGO
     JMP RODADA_XIS
-FIM:
-    IMP_STRING VITORIA
+
+; AQUI MOSTRARA A OPCAO DE JOGAR NOVAMENTE OU ESCOLHER OUTRO MODO (VOLTAR PAGINA INICIAL)
+; (VOLTAR PAGINA INICIAL)
+FIM_DE_JOGO:
+
     RET
 
 JOGADOR_X_JOGADOR ENDP
@@ -140,44 +157,6 @@ JOGADOR_X_JOGADOR ENDP
 JOGADOR_X_COMPUTADOR PROC ;(Jogador x Computador)
     RET
 JOGADOR_X_COMPUTADOR ENDP
-
-IMPRIME_TAB_INI PROC
-    ; IMPRIME A MATRIZ DO TABULEIRO
-    ; ENTRADA - BX O OFFSET DA MATRIZ, CH NUMERO DE LINHAS E DH NUMERO DE COLUNAS
-    ; SAIDA - MATRIZ IMPRESSA
-
-    PUSH_ALL
-
-    MOV AH,2 ; COMANDO IMPRESSÃO
-    LEA BX, TABULEIRO
-    XOR DH, DH ;  PARAMETROS DO TABULEIRO
-    MOV CH, 3 ; # LINHA
-    XOR BX,BX ; INDICE COLUNA
-
-    COLUNA: ; IMPRIME UMA COLUNA
-    MOV CL, 2 ; # COLUNA
-    XOR SI,SI ; INDICE LINHA
-
-    LINHA: ; IMPIRME UMA LINHA
-    MOV DL, DH  ; LEIO ELEMENTO DA MATRIZ
-    INT 21H ; IMPRIMO
-    MOV DL, ' ' ; ESPAÇO PROXIMO CARACTER
-    INT 21H ; IMPRIMO
-    INC SI ; PROXIMA COLUNA
-    DEC CL
-    INC DH
-    JNZ LINHA
-
-    MOV DL, 10
-    INT 21H
-    ADD BX,3 ; PROXIMA LINHA
-    DEC CH
-    JNZ COLUNA
-
-    POP_ALL
-
-    RET
-IMPRIME_TAB_INI ENDP
 
 IMPRIME_TAB PROC
     ; IMPRIME A MATRIZ DO TABULEIRO
@@ -193,11 +172,11 @@ IMPRIME_TAB PROC
     MOV CH, 3 ; # LINHA
     XOR BX,BX ; INDICE COLUNA
 
-COLUNAA: ; IMPRIME UMA COLUNA
+COLUNA: ; IMPRIME UMA COLUNA
     MOV CL,DH ; # COLUNA
     XOR SI,SI ; INDICE LINHA
 
-LINHAA: ; IMPIRME UMA LINHA
+LINHA: ; IMPIRME UMA LINHA
     MOV DL,[BX][SI] ; LEIO ELEMENTO DA MATRIZ
     INT 21H ; IMPRIMO
     MOV DL, ' ' ; ESPAÇO PROXIMO CARACTER
@@ -217,273 +196,171 @@ LINHAA: ; IMPIRME UMA LINHA
     RET
 IMPRIME_TAB ENDP
 
-LER_JOGADA_X PROC
+LER_JOGADA PROC
 ; LE A JOGADA DO USUARIO E ATUALIZA A MATRIZ
     PUSH_ALL
-INICIO_X:
-    MOV AH,1 ; COMANDO LEITURA
-    INT 21H
-LE_JOGADA:
-    CMP AL, '1'
-    JE POS_1
-    CMP AL, '2'
-    JE POS_2
-    CMP AL, '3'
-    JE POS_3
-    CMP AL, '4'
-    JE POS_4
-    CMP AL, '5'
-    JE POS_5
-    CMP AL, '6'
-    JE POS_6
-    CMP AL, '7'
-    JE POS_7
-    CMP AL, '8'
-    JE POS_8
-    CMP AL, '9'
-    JE POS_9
-    JMP INICIO ; CASO SEJA INVALIDO, LER NOVAMENTE
 
-POS_1:
-    MOV BX, 0
-    MOV SI, 0
-    MOV DL, 'X'
-    XCHG [BX][SI], DL 
+
+TURNO:
+    MOV AH, 1
+    LIMPA_TELA
+    CALL IMPRIME_TAB
+
+    CMP DL, 'X'
+    JE RODADA_X
+
+RODADA_O:
+    IMP_STRING TURNO_CIRCULO
+    INT 21H
+    JMP VALIDA_1a9
+
+RODADA_X:
+    IMP_STRING TURNO_XIS
+    INT 21H
+
+VALIDA_1a9:
+    CMP AL, '1'
+    JB  CASA_INVALIDA
+    CMP AL, '9'
+    JA  CASA_INVALIDA
+    JMP EXECUCAO
+
+CASA_INVALIDA:
+    IMP_STRING quebralinha
+    IMP_STRING quebralinha
+    IMP_STRING INVALIDO
+    INT 21H
+    JMP TURNO
+
+EXECUCAO:
+    LEA BX, TABULEIRO
+    SUB AL, '1' ; TRANSFORMA O INPUT EM INDICE DA CASA
+    XOR AH, AH ; RESETA PARA DIVISAO
+
+    MOV BL,3 ; DEFINE O DIVIDENDO
+    DIV BL
+    MOV SI, AX
+    SHR SI, 8 ; INDICE DA COLUNA CONCLUIDO
+
+    MUL BL ; MULTIPLICO PARA RESULTAR EM INDICE DA LINHA
+    MOV BX, AX ; INDICE DA LINHA CONCLUIDO
+
+; VALIDA SE JA FOI JOGADA
+    MOV AL, [BX][SI]
+    CMP AL, '9'
+    JA REPETIDO
     JMP FIM_DA_RODADA
-POS_2:
-    MOV BX, 0
-    MOV SI, 1
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA 
-POS_3:
-    MOV BX, 0
-    MOV SI, 2
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA
-POS_4:
-    MOV BX, 3
-    MOV SI, 0
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA
-POS_5:
-    MOV BX, 3
-    MOV SI, 1
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA
-POS_6:
-    MOV BX, 3
-    MOV SI, 2
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA
-POS_7:
-    MOV BX, 6
-    MOV SI, 0
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA
-POS_8:
-    MOV BX, 6
-    MOV SI, 1
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA
-POS_9:
-    MOV BX, 6
-    MOV SI, 2
-    MOV DL, 'X'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA
+
+; INDICA AOS JOGADDORES QUE JA FOI JOGADA A CASA SELECIONADA
+REPETIDO:
+    IMP_STRING quebralinha
+    IMP_STRING quebralinha
+    IMP_STRING REPETIDA
+    MOV AH,1 
+    INT 21H
+    JMP TURNO ; RETORNA
 
 FIM_DA_RODADA:
+    XCHG DL, [BX][SI]  
+
     POP_ALL
     RET
-LER_JOGADA_X ENDP
 
-LER_JOGADA_O PROC
-; LE A JOGADA DO USUARIO E ATUALIZA A MATRIZ
-    PUSH_ALL
-INI:
-    MOV AH,1 ; COMANDO LEITURA
-    INT 21H
-LE_JOGADA_O:
-    CMP AL, '1'
-    JE POSI_1
-    CMP AL, '2'
-    JE POSI_2
-    CMP AL, '3'
-    JE POSI_3
-    CMP AL, '4'
-    JE POSI_4
-    CMP AL, '5'
-    JE POSI_5
-    CMP AL, '6'
-    JE POSI_6
-    CMP AL, '7'
-    JE POSI_7
-    CMP AL, '8'
-    JE POSI_8
-    CMP AL, '9'
-    JE POSI_9
-    JMP INI ; CASO SEJA INVALIDO, LER NOVAMENTE
+LER_JOGADA ENDP
 
-POSI_1:
-    MOV BX, 0
-    MOV SI, 0
-    MOV DL, 'O'
-    XCHG [BX][SI], DL 
-    JMP FIM_DA_RODADA_O
-POSI_2:
-    MOV BX, 0
-    MOV SI, 1
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
-POSI_3:
-    MOV BX, 0
-    MOV SI, 2
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
-POSI_4:
-    MOV BX, 3
-    MOV SI, 0
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
-POSI_5:
-    MOV BX, 3
-    MOV SI, 1
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
-POSI_6:
-    MOV BX, 3
-    MOV SI, 2
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
-POSI_7:
-    MOV BX, 6
-    MOV SI, 0
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
-POSI_8:
-    MOV BX, 6
-    MOV SI, 1
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
-POSI_9:
-    MOV BX, 6
-    MOV SI, 2
-    MOV DL, 'O'
-    XCHG [BX][SI], DL
-    JMP FIM_DA_RODADA_O
 
-FIM_DA_RODADA_O:
-    POP_ALL
-    RET
-LER_JOGADA_O ENDP
+
+
+
+
 
 WIN_CHECK PROC
     PUSH_ALL
-    XOR BX,BX
+    LEA BX, TABULEIRO
     XOR SI,SI
 
-TEST_12:
+TEST_123:
     MOV AL, [BX][SI]       
     MOV DL, [BX][SI+1]     
     CMP AL, DL             
-
-    JE TEST_23
-
-TEST_14:
-    MOV AL, [BX][SI]       
-    MOV DL, [BX+3][SI]     
-    CMP AL, DL 
-
-    JE TEST_47
-
-TEST_159:
-    MOV AL, [BX][SI]       
-    MOV DL, [BX+3][SI+1]     
-    CMP AL, DL 
-    JNE TEST_35
-    MOV AL, [BX+3][SI+1]       
-    MOV DL, [BX+6][SI+2]     
-    CMP AL, DL 
-    JE WIN
-
-TEST_23:
+    JNE TEST_456
     MOV AL, [BX][SI+1]       
     MOV DL, [BX][SI+2]     
     CMP AL, DL 
-    JE WIN
-
-TEST_47:
-    MOV AL, [BX+3][SI]       
-    MOV DL, [BX+6][SI]     
-    CMP AL, DL 
-
-
-
-TEST_35:
-    MOV AL, [BX][SI]       
-    MOV DL, [BX][SI+1]     
-    CMP AL, DL 
-    JE TEST_57
-
-TEST_369:
-    MOV AL, [BX][SI+2]       
-    MOV DL, [BX+3][SI+2]     
-    CMP AL, DL 
-    JNE TEST_789
-    MOV AL, [BX+3][SI+2]       
-    MOV DL, [BX+6][SI+2]     
-    CMP AL, DL 
-    JE WIN
-
-TEST_57: 
-    MOV AL, [BX+3][SI+1]       
-    MOV DL, [BX+6][SI]     
-    CMP AL, DL 
-    JE WIN
-
-TEST_789:
-    MOV AL, [BX+6][SI]       
-    MOV DL, [BX+6][SI+1]     
-    CMP AL, DL 
     JNE TEST_456
-    MOV AL, [BX+6][SI+1]       
-    MOV DL, [BX+6][SI+2]      
-    CMP AL, DL 
-    JE WIN
+    JMP WINNER
 
 TEST_456:
     MOV AL, [BX+3][SI]       
     MOV DL, [BX+3][SI+1] 
     CMP AL, DL   
-    JNE TEST_258
+    JNE TEST_789
     MOV AL, [BX+3][SI+1]       
     MOV DL, [BX+3][SI+2] 
     CMP AL, DL
-    JE WIN   
+    JNE TEST_789
+    JMP WINNER   
+
+TEST_789:
+    MOV AL, [BX+6][SI]       
+    MOV DL, [BX+6][SI+1]     
+    CMP AL, DL 
+    JNE TEST_147
+    MOV AL, [BX+6][SI+1]       
+    MOV DL, [BX+6][SI+2]      
+    CMP AL, DL 
+    JE WIN
+
+TEST_147:
+    MOV AL, [BX][SI]       
+    MOV DL, [BX+3][SI]     
+    CMP AL, DL
+    JNE TEST_258
+    MOV AL, [BX+3][SI]       
+    MOV DL, [BX+6][SI]     
+    CMP AL, DL
+    JE WIN  
 
 TEST_258:
     MOV AL, [BX][SI+1]       
     MOV DL, [BX+3][SI+1] 
     CMP AL, DL   
-    JNE NO_WIN
+    JNE TEST_369
     MOV AL, [BX+3][SI+1]       
     MOV DL, [BX+6][SI+1] 
     CMP AL, DL
-    JE WIN   
+    JE WIN 
+
+TEST_369:
+    MOV AL, [BX+2][SI]       
+    MOV DL, [BX+3][SI+2] 
+    CMP AL, DL   
+    JNE TEST_159
+    MOV AL, [BX+3][SI+2]       
+    MOV DL, [BX+6][SI+2] 
+    CMP AL, DL
+    JE WIN 
+
+
+TEST_159:
+    MOV AL, [BX][SI]       
+    MOV DL, [BX+3][SI+1]     
+    CMP AL, DL 
+    JNE TEST_357
+    MOV AL, [BX+3][SI+1]       
+    MOV DL, [BX+6][SI+2]     
+    CMP AL, DL 
+    JE WIN
+
+TEST_357:
+    MOV AL, [BX][SI+2]       
+    MOV DL, [BX+3][SI+1]     
+    CMP AL, DL 
+    JNE NO_WIN
+    MOV AL, [BX+3][SI+1]       
+    MOV DL, [BX+6][SI]     
+    CMP AL, DL 
+    JE WIN
 
 NO_WIN:
 ; Não houve vitória
@@ -492,15 +369,25 @@ NO_WIN:
     CMP AX, AX      ; ZF=1
     RET
 
+WINNER: 
 WIN:
 ; Houve vitória
-    POP_ALL
+    CMP DL, 'X'
+    JE VIT_X 
+VIT_O:
+    IMP_STRING VITORIA_CIRCULO
     MOV AX, 1
     CMP AX, 0
+    POP_ALL
+    RET   
+
+VIT_X:
+    IMP_STRING VITORIA_XIS
+    MOV AX, 1
+    CMP AX, 0
+    POP_ALL
     RET  
 
 WIN_CHECK ENDP
-
-
 
 END MAIN
